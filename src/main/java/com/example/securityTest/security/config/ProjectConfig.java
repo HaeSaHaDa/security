ac08e9641cd8c1be97d92c3d94ae5b4161220ee1
sixthgkrsus <sixthgkrsus@naver.com>
2025-03-14 14:44:05 +0900

    start

---
+++ src/main/java/com/example/securityTest/security/config/ProjectConfig.java
@@ -0,0 +1,29 @@
+package com.example.securityTest.security.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+
+
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        var userDetailService = new InMemoryUserDetilsManager();
+//
+//
+//        UserDetails user1 = new User("sss", "12345", List.of(new SimpleGrantedAuthority("ROLE_USER")));
+//
+//        var user = User.withUserDetails(user1)
+//                .build();
+//
+//        userDetailService.createUser(user);
+//
+//        return userDetailService;
+//    }
+//
+
+
+
+}
---
+++ src/main/java/com/example/securityTest/security/config/WebAuthorizationConfig.java
@@ -0,0 +1,22 @@
+package com.example.securityTest.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class WebAuthorizationConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll())
+                .formLogin(withDefaults());  // 기본 로그인 폼 사용
+
+        return http.build();
+    }
+}
